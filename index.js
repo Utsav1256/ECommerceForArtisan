f880b3a0ecaf70201f4dd3f006b08d4ddd7d01ba
Utsav <utsavkumar5462@gmail.com>
2023-01-08 00:08:33 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+//requiring express in the entry point file 
 const express = require('express');
 const app = express();
 const port = 8000;
 
-app.listen(posrt, function(err){
+app.listen(post, function(err){
+    // instead of using ',' or '+' for concatenating two string, we can embed our variable inside {} the string using (``)
     if(err) {
         console.log(`Error in running the server : ${err}`);
     }
     console.log(`Server is runnning on posrt : ${port}`);
 });
+
