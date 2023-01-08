15d723ad1fd8bf29cb3c72558dc84ddf02eb1534
Utsav <utsavkumar5462@gmail.com>
2023-01-08 15:03:56 +0530

    set up the express router

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.listen(post, function(err){
+
+//  using express router
+app.use('/', require('./routes'));
+
+
+app.listen(port, function(err) {
     // instead of using ',' or '+' for concatenating two string, we can embed our variable inside {} the string using (``)
     if(err) {
         console.log(`Error in running the server : ${err}`);
     }
-    console.log(`Server is runnning on posrt : ${port}`);
+    console.log(`Server is runnning on port : ${port}`);
 });
 
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+// it is the entry point of all the routes
+const express = require('express'); 
+const router = express.Router(); // express.Router() is a module, which will help in separating the app routes and controller
+console.log('router loaded'); //    checking if router is loaded or not
+module.exports = router;
