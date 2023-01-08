65eba1df973db7261a8df4ab8c774bd6576a8f8c
Utsav <utsavkumar5462@gmail.com>
2023-01-08 20:38:08 +0530

    Render pages for signup and sign in

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,9 @@ console.log('users router loaded');
 const usersController = require('../controllers/users_controller');
 // accessing 'users_controller action' -> that is 'profile' 
 router.get('/profile', usersController.profile);
-
+// accessing 'users_controller action' -> that is 'signUp'
+router.get('/sign-up', usersController.signUp);
+// accessing 'users_controller action' -> that is 'signIn'
+router.get('/sign-in', usersController.signIn);
 
 module.exports = router;
