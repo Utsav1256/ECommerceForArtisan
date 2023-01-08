257455dcad6b30d5c07881ea6caddc2ec96ab403
Utsav <utsavkumar5462@gmail.com>
2023-01-08 15:22:34 +0530

    create home action in home_controller inside controllers

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,15 @@
 const express = require('express'); 
 const router = express.Router(); // express.Router() is a module, which will help in separating the app routes and controller
 console.log('router loaded'); //    checking if router is loaded or not
+
+// requiring the home_controller
+const homeController = require('../controllers/home_controller');
+// accessing 'home_controller action' -> which is 'home' 
+router.get('/', homeController.home);
+
+
+
+
+
+
 module.exports = router;
