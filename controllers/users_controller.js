ffdcd7db1aefae87461f993f5b084087ab6c0eee
Utsav <utsavkumar5462@gmail.com>
2023-01-08 16:33:41 +0530

    create a view for users_profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile = function(request, response) {
-    return response.end('<h1>User Profile</h1>');
+    return response.render('users_profile', {
+        title: "User profile"
+    });
 }
